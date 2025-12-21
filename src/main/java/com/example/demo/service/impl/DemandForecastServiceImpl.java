7e7f6c84ee75763e3c48f56034526af08702aa7b
bala-amypo <bala@amypo.in>
2025-12-21 15:20:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/DemandForecastServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo.service.impl;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.DemandForecast;
+import com.example.demo.repository.DemandForecastRepository;
+import com.example.demo.service.DemandForecastService;
+
+@Service
+public class DemandForecastServiceImpl implements DemandForecastService {
+
+    private final DemandForecastRepository demandForecastRepository;
+
+    public DemandForecastServiceImpl(DemandForecastRepository demandForecastRepository) {
+        this.demandForecastRepository = demandForecastRepository;
+    }
+
+    @Override
+    public DemandForecast saveForecast(DemandForecast forecast) {
+        return demandForecastRepository.save(forecast);
+    }
+
+    @Override
+    public List<DemandForecast> getAllForecasts() {
+        return demandForecastRepository.findAll();
+    }
+}
