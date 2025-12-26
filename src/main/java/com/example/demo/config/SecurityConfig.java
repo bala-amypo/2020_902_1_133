f26a0f15db47686e552b9116f609441b0a4576a0
bala-amypo <bala@amypo.in>
2025-12-26 16:50:58 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,16 +1,3 @@
-package com.example.demo.config;
-
-import com.example.demo.service.CustomUserDetailsService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -23,25 +10,23 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
             .csrf(csrf -> csrf.disable())
+
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/h2-console/**", "/login").permitAll()
                 .anyRequest().authenticated()
             )
+
             .userDetailsService(userDetailsService)
+
             .formLogin(form -> form
                 .defaultSuccessUrl("/", true)
                 .permitAll()
             )
+
             .logout(logout -> logout.permitAll());
 
         http.headers(headers ->
