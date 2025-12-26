335b55db942450ac5814d0d14283057b3a5219f7
bala-amypo <bala@amypo.in>
2025-12-26 08:12:17 +0000

    User commit

--- src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -3,13 +3,10 @@ package com.example.demo.service;
 import com.example.demo.model.UserAccount;
 import com.example.demo.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -21,8 +18,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserAccount user = userAccountRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        return new User(user.getUsername(), user.getPassword(),
-                Collections.singleton(() -> user.getRole()));
+        return user;
     }
 }
