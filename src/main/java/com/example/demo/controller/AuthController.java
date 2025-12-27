2e6430f42f7969fecc687999a8343d29aead8d48
bala-amypo <bala@amypo.in>
2025-12-27 04:17:18 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -26,4 +26,3 @@ public class AuthController {
         return authService.login(request);
     }
 }
-
--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -14,26 +14,24 @@ public class UserAccount {
     private String email;
 
     private String password;
-
     private String role;
 
-    // ✅ REQUIRED getters & setters
     public Long getId() {
         return id;
     }
-
+ 
     public void setId(Long id) {
         this.id = id;
     }
-
+ 
     public String getEmail() {
         return email;
     }
-
+ 
     public void setEmail(String email) {
         this.email = email;
     }
-
+ 
     public String getPassword() {
         return password;
     }
