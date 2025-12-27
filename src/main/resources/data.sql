84dce36a2a0093b387007648805479e43360d7b5
bala-amypo <bala@amypo.in>
2025-12-27 03:35:17 +0000

    User commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (id, username, password)
+INSERT INTO users (email, password, role)
 VALUES (
-  1,
-  'admin',
-  '$2a$10$7s8gZlCz0xg7y9hHc8vM7uK2UQHn1x0z2xQkq9J7mJ3Zz5o6z6p8m'
+  'admin@gmail.com',
+  '$2a$10$7s8gZlCz0xg7y9hHc8vM7uK2UQHn1x0z2xQkq9J7mJ3Zz5o6z6p8m',
+  'ROLE_ADMIN'
 );
