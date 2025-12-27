ac7b25b42b250c1096ba9abbd6bc397bcca4b76c
bala-amypo <bala@amypo.in>
2025-12-27 04:05:44 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,12 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.AuthRequestDto;
-import com.example.demo.dto.AuthResponseDto;
-import com.example.demo.dto.RegisterRequestDto;
+import com.example.demo.entity.UserAccount;
 
 public interface AuthService {
 
-    void register(RegisterRequestDto request);
-
-    AuthResponseDto login(AuthRequestDto request);
+    UserAccount register(String email, String password);
 }
