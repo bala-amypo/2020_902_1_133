45ead7df4715af279b3cb07222c417ed9414871e
bala-amypo <bala@amypo.in>
2025-12-28 10:06:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/TransferSuggestionRepository.java
+++ src/main/java/com/example/demo/repository/TransferSuggestionRepository.java
@@ -1,10 +1,23 @@
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.TransferSuggestion;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import java.util.List;
+
+// public interface TransferSuggestionRepository extends JpaRepository<TransferSuggestion, Long> {
+//     List<TransferSuggestion> findBySourceStoreId(Long storeId);
+//     List<TransferSuggestion> findByProduct_Id(Long productId);
+// }
+
+
 package com.example.demo.repository;
 
 import com.example.demo.entity.TransferSuggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface TransferSuggestionRepository extends JpaRepository<TransferSuggestion, Long> {
-    List<TransferSuggestion> findBySourceStoreId(Long storeId);
-    List<TransferSuggestion> findByProduct_Id(Long productId);
+public interface TransferSuggestionRepository
+        extends JpaRepository<TransferSuggestion, Long> {
+
+    List<TransferSuggestion> findByProductId(Long productId);
 }
